fc9d074db18588030f79a5fe9c7016e21b29eeeb
yaxuanpang <yaxuanpang0331@gmail.com>
2025-09-04 22:25:27 -0400

    implemented the greatest record of all time

--- topics/instructions/drawing-practice/js/script.js
+++ topics/instructions/drawing-practice/js/script.js
@@ -1,24 +1,45 @@
 /**
- * Title of Project
- * Author Name
+ * Drawing Module
+ * Yaxuan Pang
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Practicing and learning drawing functions in p5.
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Creates the canvas
 */
 function setup() {
-
+    // A nice square canvas to work with
+    createCanvas(640, 640);
 }
 
-
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
+ * Draws a bright red record
+ */
 function draw() {
+    // A grey background
+    background(150, 150, 150);
 
+    // The main part of the record is red
+    push();
+    fill(255, 0, 0);
+    stroke(255, 255, 255);
+    ellipse(320, 320, 480, 480);
+    pop();
+
+    // The label on the record
+    push();
+    fill(255, 255, 255);
+    noStroke();
+    ellipse(320, 320, 140, 140);
+    pop();
+
+    // The hole in the record
+    push();
+    fill(150, 150, 150);
+    stroke(50, 50, 50);
+    ellipse(320, 320, 20, 20);
+    pop();
 }
