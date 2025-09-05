ff011d771b850f67420568773599f53ea2e41197
yaxuanpang <yaxuanpang0331@gmail.com>
2025-09-04 21:33:03 -0400

    Finished simulation of writer with writer's block

--- topics/instructions/function-calls/js/script.js
+++ topics/instructions/function-calls/js/script.js
@@ -1,24 +1,19 @@
 /**
- * Title of Project
- * Author Name
+ * Function Calls
+ * Yaxuan Pang
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Working on the Function Calls examples.
+
  */
 
 "use strict";
 
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
 function setup() {
-
+    createCanvas(640, 480);
 }
 
 
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
 function draw() {
-
+    background(255, 100, 100);
+    rect(200, 80, 240, 320);
 }
