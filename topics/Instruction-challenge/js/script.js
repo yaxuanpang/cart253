bc422618078819a5a9846dfb17406840c83b01f5
yaxuanpang <yaxuanpang0331@gmail.com>
2025-09-11 16:19:48 -0400

    created windows and antenna
    different types of windows were made and we added waves to the water. we added the antennas.

--- topics/Instruction-challenge/js/script.js
+++ topics/Instruction-challenge/js/script.js
@@ -33,10 +33,6 @@ function draw() {
     pop();
 
     /**
-     * create skyscrapers
-     */
-
-    /**
      * creating skyscraper 1
      */
 
@@ -57,12 +53,134 @@ function draw() {
     pop();
 
     /**
-     * waves
+     * waves 1
      */
 
     push();
     noStroke();
     fill(26, 37, 107);
     rect(350, 450, 100, 10);
+    pop();
+
+    /**
+     * waves 2
+     */
+
+    push();
+    noStroke();
+    fill(26, 37, 107);
+    rect(10, 410, 100, 10);
+    pop();
+
+
+    /**
+    * waves 3
+    */
+
+    push();
+    noStroke();
+    fill(26, 37, 107);
+    rect(30, 445, 100, 10);
+    pop();
+
+    /**
+   * waves 4
+   */
+
+    push();
+    noStroke();
+    fill(26, 37, 107);
+    rect(150, 480, 100, 10);
+    pop();
+
+
+    /**
+   * waves 5
+   */
+
+    push();
+    noStroke();
+    fill(26, 37, 107);
+    rect(200, 425, 100, 10);
+    pop();
+
+
+    /**
+   * waves 6
+   */
+
+    push();
+    noStroke();
+    fill(26, 37, 107);
+    rect(450, 420, 100, 10);
+    pop();
+
+    /**
+     * window seperation 1
+     */
+
+    push();
+    noStroke();
+    fill(209, 212, 227);
+    rect(300, 150, 100, 10);
+    pop();
+
+    /**
+  * window seperation 2
+  */
+
+    push();
+    noStroke();
+    fill(209, 212, 227);
+    rect(125, 150, 100, 10);
+    pop();
+
+    /**
+     * window seperation 3
+     */
+
+    push();
+    noStroke();
+    fill(209, 212, 227);
+    rect(300, 250, 100, 10);
+    pop();
+
+    /**
+  * window seperation 4
+  */
+
+    push();
+    noStroke();
+    fill(209, 212, 227);
+    rect(125, 250, 100, 10);
+    pop();
+
+    /**
+     * building antenna
+     */
+
+    push();
+    noStroke();
+    fill(209, 212, 227);
+    rect(170, 0, 10, 50);
+    pop();
+
+    /**
+  * window 1
+  */
+
+    push();
+    stroke(83, 88, 120);
+    fill(94, 100, 133);
+    rect(145, 170, 30, 30);
+    pop();
+
+    /**
+ * window 2
+ */
+    push();
+    stroke(83, 88, 120);
+    fill(94, 100, 133);
+    rect(355, 270, 30, 30);
     pop();
 }
