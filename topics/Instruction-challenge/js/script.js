9b82474a339d1b9e9043f9ea0110a545b68b1187
yaxuanpang <yaxuanpang0331@gmail.com>
2025-09-11 15:34:22 -0400

    created the ground (cement)

--- topics/Instruction-challenge/js/script.js
+++ topics/Instruction-challenge/js/script.js
@@ -12,14 +12,23 @@
  * In a possible dystopian future.
 */
 function setup() {
-    CreateCanvas(500, 500);
+    createCanvas(500, 500);
 }
 
 
 /**
- * skyscrapers on a cement.
+ * setting up the background whihc is the sky
 */
 function draw() {
     background(12, 48, 133);
 
+
+    /**
+     * create cement
+     */
+    push();
+    noStroke();
+    fill(182, 196, 219);
+    rect(0, 400, 500, 100);
+    pop();
 }
