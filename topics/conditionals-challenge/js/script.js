b0ccfb665c02262de805cc35374ed4f7e26a6c2f
yaxuanpang <yaxuanpang0331@gmail.com>
2025-09-25 15:03:02 -0400

    set up the example code

--- topics/conditionals-challenge/js/script.js
+++ topics/conditionals-challenge/js/script.js
@@ -1,18 +1,33 @@
 /**
- * Title of Project
- * Author Name
+ * Puck
+ * Yaxuan, ashmitha, Nerly
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ *  This will be a program in which the user can push a circle
+ * on the canvas using their own circle.
  */
 
 "use strict";
 
+const puck = {
+    x: 200,
+    y: 200,
+    size: 100,
+    fill: "#ff0000"
+};
+
+const user = {
+    x: undefined, // will be mouseX
+    y: undefined, // will be mouseY
+    size: 75,
+    fill: "#000000"
+};
+
+
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Create canvas
 */
 function setup() {
-
+    createCanvas(400, 400);
 }
 
 
@@ -20,5 +35,42 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+    background("#aaaaaa");
 
+    // Move user circle
+    moveUser();
+
+    // Draw the user and puck
+    drawUser();
+    drawPuck();
+}
+
+/**
+ * Sets the user position to the mouse position
+ */
+function moveUser() {
+    user.x = mouseX;
+    user.y = mouseY;
+}
+
+/**
+ * Displays the user circle
+ */
+function drawUser() {
+    push();
+    noStroke();
+    fill(user.fill);
+    ellipse(user.x, user.y, user.size);
+    pop();
+}
+
+/**
+ * Displays the puck circle
+ */
+function drawPuck() {
+    push();
+    noStroke();
+    fill(puck.fill);
+    ellipse(puck.x, puck.y, puck.size);
+    pop();
 }
