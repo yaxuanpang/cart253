c89d19e88fd0123438a63137cbb21a7afc197b62
yaxuanpang <yaxuanpang0331@gmail.com>
2025-09-27 03:59:44 -0400

    combined the elements for the eyes

--- topics/art-jam/js/script.js
+++ topics/art-jam/js/script.js
@@ -25,48 +25,93 @@ function draw() {
     drawFace(); //drawing the head
     drawBangs(); //drawing the bangs
     drawEyes(); //drawing the eyes
-
-    function drawFace() {
-        //drawing the face
-        push();
-        noStroke();
-        fill(255, 240, 201);
-        circle(width / 2, 250, 250);
-        pop();
-    }
-
-    function drawHair() {
-        //drawing the hair behind the head
-        push();
-        noStroke();
-        fill(0);
-        rect(225, 250, 250, 250);
-        pop();
-    }
-
-    function drawBangs() {
-        //drawing the bangs
-        push();
-        noStroke();
-        fill(0);
-        // making the half circle for the bangs
-        arc(350, 250, 250, 250, 180, 360);
-        pop();
-    }
-
-    function drawEyes() {
-        push();
-        noStroke();
-        fill(255);
-        circle(287, 275, 30); //white of left eye
-        circle(412, 275, 30); //white of right eye
-        fill(0);
-        circle(287, 275, 10); //iris of left eye
-        circle(412, 275, 10); //iris of right eye
-        fill(255);
-        circle(290, 272, 3); //shine of left eye
-        circle(415, 272, 3); //shine of right eye
-        pop();
-    }
-
+    drawPupils();
 }
+function drawFace() {
+    //drawing the face
+    push();
+    noStroke();
+    fill(255, 240, 201);
+    circle(width / 2, 250, 250);
+    pop();
+}
+
+function drawHair() {
+    //drawing the hair behind the head
+    push();
+    noStroke();
+    fill(0);
+    rect(225, 250, 250, 250);
+    pop();
+}
+
+function drawBangs() {
+    //drawing the bangs
+    push();
+    noStroke();
+    fill(0);
+    // making the half circle for the bangs
+    arc(350, 250, 250, 250, 180, 360);
+    pop();
+}
+
+function drawEyes() {
+    push();
+    noStroke();
+    fill(255);
+    circle(287, 275, 30); //white of left eye
+    circle(412, 275, 30); //white of right eye
+    pop();
+}
+
+function drawPupils() {
+    push();
+    stroke(94, 42, 7); //gave myself dark brown eyes :)
+    strokeWeight(3.5); //iris
+    fill(0);
+    circle(287, 275, 16); //pupils of left eye
+    circle(412, 275, 16); //pupils of right eye
+    noStroke();
+    fill(255);
+    circle(290, 272, 3); //shine of left eye
+    circle(415, 272, 3); //shine of right eye
+    pop();
+}
+
+
+// if (mouseY >= 260 & mouseY <= 290) {
+//     if (mouseX >= 242 & mouseX <= 272) {
+//         circle(mouseX, mouseY, 10);
+//     }
+//     else if (mouseX < 242) {
+//         circle(242, mouseY, 10);
+//     }
+//     else if (mouseX > 272) {
+//         circle(272, mouseY, 10);
+//     }
+// }
+
+
+// else if (mouseY < 260) {
+//     if (mouseX > 225 & mouseX < 475) {
+//         circle(mouseX, 0, 100, 100, 0, 180);
+//     }
+//     else if (mouseX < 225) {
+//         circle(225, 0, 100, 100, 0, 180);
+//     }
+//     else if (mouseX > 290) {
+//         circle(475, 0, 100, 100, 0, 180);
+//     }
+// }
+
+// else {
+//     if (mouseX > 225 & mouseX < 475) {
+//         circle(mouseX, 400, 100, 100, 0, 180);
+//     }
+//     else if (mouseX < 225) {
+//         circle(225, 400, 100, 100, 0, 180);
+//     }
+//     else if (mouseX > 475) {
+//         circle(475, 400, 100, 100, 0, 180);
+//     }
+// }
