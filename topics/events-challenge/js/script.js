770309eb0c223541d2a8ec697caa42ddcb6598f6
yaxuanpang <yaxuanpang0331@gmail.com>
2025-10-02 15:32:24 -0400

    finished the events challenge

--- topics/events-challenge/js/script.js
+++ topics/events-challenge/js/script.js
@@ -26,6 +26,8 @@ function setup() {
 function draw() {
     background("#87ceeb");
 
+    center();
+
     // Only increase the score if the game is not over
     if (!gameOver) {
         // Score increases relatively slowly
@@ -64,15 +66,32 @@ function displayScore() {
     text(floor(score), width / 2, height / 2);
     pop();
 }
-
+//function that calls sets gameOver to true, which displays the you lose! text
 function lose() {
     gameOver = true;
 
 }
-
+//to check if the "a" or "A" is being pressed
 function keyPressed(event) {
 
     if (event.keyCode === 65) {
         lose();
     }
+}
+
+//if the user clicks on the number they lose
+function mousePressed() {
+    if (mouseX, mouseY, width / 2, height / 2) {
+        lose();
+    }
+
+}
+
+//creating the circle where we will then check if mouse is being clicked inside
+function center() {
+    push();
+    noStroke();
+    fill("#87ceeb");
+    circle(width / 2, height / 2, 50);
+    pop();
 }
