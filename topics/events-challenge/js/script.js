ecffa13c4349f86d434e2bcd6e689b332a52b424
yaxuanpang <yaxuanpang0331@gmail.com>
2025-10-02 15:11:51 -0400

    finished step 3

--- topics/events-challenge/js/script.js
+++ topics/events-challenge/js/script.js
@@ -1,24 +1,78 @@
 /**
- * Title of Project
- * Author Name
- * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * The Only Move Is Not To Play
+ * Pippin Barr
+ *
+ * A game where your score increases so long as you do nothing.
  */
 
 "use strict";
 
+// Current score
+let score = 0;
+
+// Is the game over?
+let gameOver = false;
+
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
+ * Create the canvas
+ */
 function setup() {
+    createCanvas(400, 400);
+}
+
+/**
+ * Update the score and display the UI
+ */
+function draw() {
+    background("#87ceeb");
+
+    // Only increase the score if the game is not over
+    if (!gameOver) {
+        // Score increases relatively slowly
+        score += 0.05;
+    }
+    displayUI();
+
+
+
 
 }
 
+/**
+ * Show the game over message if needed, and the current score
+ */
+function displayUI() {
+    if (gameOver) {
+        push();
+        textSize(48);
+        textStyle(BOLD);
+        textAlign(CENTER, CENTER);
+        text("You lose!", width / 2, height / 3);
+        pop();
+    }
+    displayScore();
+}
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
-function draw() {
+ * Display the score
+ */
+function displayScore() {
+    push();
+    textSize(48);
+    textStyle(BOLD);
+    textAlign(CENTER, CENTER);
+    text(floor(score), width / 2, height / 2);
+    pop();
+}
 
+function lose() {
+    gameOver = true;
+
+}
+
+function keyPressed(event) {
+
+    if (event.keyCode === 65) {
+        lose();
+    }
 }
