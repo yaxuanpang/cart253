2f6d0711b8d2828314b5ad1e6dc524951a60e8e0
yaxuanpang <yaxuanpang0331@gmail.com>
2025-11-13 14:09:35 -0500

    started the data challenge

---
+++ topics/data-challenge/js/script.js
@@ -0,0 +1,55 @@
+/**
+ * Data Challenge
+ * Yaxuan and Ashmitha
+ * 
+ * A program to generate new car model names using dinosaurs.
+ *
+ * Uses:
+ * Darius Kazemi's corpora repository
+ * https://github.com/dariusk/corpora/tree/master
+ */
+
+"use strict";
+
+let carData = undefined;
+let dinosaurData = undefined;
+let langData = undefined;
+let lang = "fr";
+
+// Starts with the instruction
+let carName = "Click to generate a car name.";
+
+/**
+ * Load the car and dinosaur data
+ */
+function preload() {
+
+}
+
+/**
+ * Create the canvas
+*/
+function setup() {
+    createCanvas(600, 400);
+}
+
+/**
+ * Display the current main text (either instructions or a car)
+*/
+function draw() {
+    background(0);
+
+    push();
+    fill("pink");
+    textAlign(CENTER, CENTER);
+    textSize(32);
+    text(carName, width / 2, height / 2);
+    pop();
+}
+
+/**
+ * Generate a new car name
+ */
+function mousePressed() {
+
+}
